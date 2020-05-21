51fdbcaae35ab15375604862cf770491656e89af
BAH12 <abubakarrbah90@gmail.com>
2020-05-21 04:58:51 +0100

    Take two lists, say for example these two:

--- practice.py
+++ practice.py
@@ -2,12 +2,14 @@
 # Print out a message addressed to them that tells them the year that they
 # will turn 100 years old.
 
+
 print("What is Your Name ?")
 name = input()
 print("What is Your Age ?")
 age = int(input())
 year = str((2020 - age) + 100)
 print(name + " Will be 100 years old in a year " + year)
+
 
 # Ask the user for a number. Depending on whether the number is even or odd,
 # print out an appropriate message to the user.
@@ -20,8 +22,10 @@ if mod > 0:
 else:
     print("You picked an 'EVEN' Number")
 
+
  #  Ask the user for a number and return a list that contains only
  #  elements from the original list a that are smaller than that number given by the user.
+
 
 print("Choose a number from this list \n(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89):")
 list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
@@ -32,10 +36,12 @@ elif user_choice in list1:
     print("Correct, you have picked a number from the list")
     print("Thank You")
 
+
 # Create a program that asks the user for a number and then
 # prints out a list of all the divisors of that number.
 # (If you don’t know what a divisor is, it is a number that divides evenly into another number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
 
 print("Please choose a number to divide")
 num = int(input())
@@ -47,6 +53,7 @@ for number in list_Range:
 print(divisor_list)
 
 
+
 # Take two lists, say for example these two:
 #   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -54,6 +61,7 @@ print(divisor_list)
 # only the elements that are common
 # between the lists (without duplicates). Make sure your
 # program works on two lists of different sizes.
+
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
